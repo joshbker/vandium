ab577a4bc75fd768312575a84d5050acacf85b95
onlyjot <jot.enquiries@gmail.com>
2021-06-24 15:51:56 +0100

    add command framework

---
+++ src/main/java/dev/devous/vandium/command/Command.java
@@ -0,0 +1,64 @@
+package dev.devous.vandium.command;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Command {
+
+	/**
+	 * The name of the command. If it is a sub command then its values would be
+	 * separated by periods. ie. a command that would be a subcommand of test
+	 * would be 'test.subcommandname'
+	 * 
+	 * @return
+	 */
+	String name();
+
+	/**
+	 * Gets the required permission of the command
+	 * 
+	 * @return
+	 */
+	String permission() default "";
+
+	/**
+	 * The message sent to the player when they do not have permission to
+	 * execute it
+	 * 
+	 * @return
+	 */
+	String noPerm() default "&cYou do not have permission to do this.";
+
+	/**
+	 * A list of alternate names that the command is executed under. See
+	 * name() for details on how names work
+	 * 
+	 * @return
+	 */
+	String[] aliases() default {};
+
+	/**
+	 * The description that will appear in /help of the command
+	 * 
+	 * @return
+	 */
+	String description() default "";
+
+	/**
+	 * The usage that will appear in /help (commandname)
+	 * 
+	 * @return
+	 */
+	String usage() default "";
+	
+	/**
+	 * Whether or not the command is available to players only
+	 * 
+	 * @return
+	 */
+	boolean inGameOnly() default false;
+}
---
+++ src/main/java/dev/devous/vandium/command/CommandArgs.java
@@ -0,0 +1,96 @@
+package dev.devous.vandium.command;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CommandArgs {
+
+	private final CommandSender sender;
+	private final org.bukkit.command.Command command;
+	private final String[] args;
+
+	protected CommandArgs(CommandSender sender, org.bukkit.command.Command command, String label, String[] args,
+                          int subCommand) {
+		String[] modArgs = new String[args.length - subCommand];
+		if (args.length - subCommand >= 0) System.arraycopy(args, subCommand, modArgs, 0, args.length - subCommand);
+
+		StringBuilder builder = new StringBuilder();
+		builder.append(label);
+
+		for (int x = 0; x < subCommand; x++)
+			builder.append(".").append(args[x]);
+
+
+		String cmdLabel = builder.toString();
+		this.sender = sender;
+		this.command = command;
+		this.args = modArgs;
+	}
+
+	/**
+	 * Gets the command sender
+	 * 
+	 * @return
+	 */
+	public CommandSender getSender() {
+		return sender;
+	}
+
+	/**
+	 * Gets the original command object
+	 * 
+	 * @return
+	 */
+	public org.bukkit.command.Command getCommand() {
+		return command;
+	}
+
+	/**
+	 * Gets the label including sub command labels of this command
+	 * 
+	 * @return Something like 'test.subcommand'
+	 */
+	public String getLabel() {
+		return command.getLabel();
+	}
+
+	/**
+	 * Gets all the arguments after the command's label. ie. if the command
+	 * label was test.subcommand and the arguments were subcommand foo foo, it
+	 * would only return 'foo foo' because 'subcommand' is part of the command
+	 * 
+	 * @return
+	 */
+	public String[] getArgs() {
+		return args;
+	}
+	
+	/**
+	 * Gets the argument at the specified index
+	 * @param index The index to get
+	 * @return The string at the specified index
+	 */
+	public String getArgs(int index) {
+		return args[index];
+	}
+
+	/**
+	 * Returns the length of the command arguments
+	 * @return int length of args
+	 */
+	public int length() {
+		return args.length;
+	}
+	
+	public boolean isPlayer() {
+		return sender instanceof Player;
+	}
+
+	public Player getPlayer() {
+		if (sender instanceof Player)
+			return (Player) sender;
+
+		return null;
+	}
+
+}
