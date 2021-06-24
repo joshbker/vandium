c5703f83426837b877b49cd4f0cf4269cd9f6b3d
onlyjot <jot.enquiries@gmail.com>
2021-06-24 16:32:58 +0100

    add thread

---
+++ src/main/java/dev/devous/vandium/threading/Thread.java
@@ -0,0 +1,44 @@
+package dev.devous.vandium.threading;
+
+import java.util.UUID;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class Thread {
+
+    private final UUID uniqueId;
+    private final String name;
+    private final ScheduledExecutorService service;
+
+    public Thread(UUID uniqueId, String name) {
+        this.uniqueId = uniqueId;
+        this.name = name;
+        service = new ScheduledThreadPoolExecutor(1);
+    }
+
+    public UUID getUniqueId() {
+        return uniqueId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void runTask(final Runnable task) {
+        service.execute(task);
+    }
+
+    public void runTaskLater(final Runnable task, final long delay, final TimeUnit unit) {
+        service.schedule(task, delay, unit);
+    }
+
+    public void runTaskTimer(final Runnable task, final long delay, final TimeUnit unit) {
+        service.scheduleAtFixedRate(task, delay, delay, unit);
+    }
+
+    public void terminate() {
+        service.shutdown();
+    }
+
+}
---
+++ src/main/java/dev/devous/vandium/threading/registry/ThreadRegistry.java
@@ -0,0 +1,66 @@
+package dev.devous.vandium.threading.registry;
+
+import dev.devous.vandium.threading.Thread;
+
+import java.util.*;
+
+public class ThreadRegistry {
+
+    private final Map<UUID, Thread> threads;
+
+    public ThreadRegistry() {
+        threads = new HashMap<>();
+    }
+
+    public Thread register(String name) {
+        Thread thread = new Thread(getUniqueId(), name);
+        threads.put(thread.getUniqueId(), thread);
+        return thread;
+    }
+
+    public Thread unregister(Thread thread) {
+        threads.remove(thread.getUniqueId());
+        thread.terminate();
+        return thread;
+    }
+
+    public Thread unregister(UUID uniqueId) {
+        Thread thread = getThread(uniqueId);
+
+        if (thread == null) {
+            return null;
+        }
+
+        threads.remove(uniqueId);
+        thread.terminate();;
+
+        return thread;
+    }
+
+    public Thread getThread(UUID uniqueId) {
+        return threads.getOrDefault(uniqueId, null);
+    }
+
+    public Thread[] getThread(String name) {
+        List<Thread> threadList = new ArrayList<>();
+
+        for (Thread thread : threads.values()) {
+            if (thread.getName().equals(name)) {
+                threadList.add(thread);
+            }
+        }
+
+        return (Thread[]) threadList.toArray();
+    }
+
+    private UUID getUniqueId() {
+        UUID uniqueId = UUID.randomUUID();
+
+        while (threads.containsKey(uniqueId)) {
+            uniqueId = UUID.randomUUID();
+        }
+
+        return uniqueId;
+    }
+
+}
